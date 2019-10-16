bb5d786f4b2ca04e28889b5c85a901d0d0c24f61
Carlos Santana <carlossantana987@outlook.com>
2019-10-16 09:43:11 -0400

    LRUcache complete

--- lru_cache/lru_cache.py
+++ lru_cache/lru_cache.py
@@ -1,3 +1,7 @@
+
+from doubly_linked_list import DoublyLinkedList
+
+
 class LRUCache:
     """
     Our LRUCache class keeps track of the max number of nodes it
@@ -7,7 +11,10 @@ class LRUCache:
     to every node stored in the cache.
     """
     def __init__(self, limit=10):
-        pass
+        self.limit = limit
+        self.size = 0
+        self.cache = {}
+        self.storage = DoublyLinkedList()
 
     """
     Retrieves the value associated with the given key. Also
@@ -17,7 +24,14 @@ class LRUCache:
     key-value pair doesn't exist in the cache.
     """
     def get(self, key):
-        pass
+        if key in self.cache:
+            i = self.cache[key]
+            self.storage.move_to_front(i)
+            return i.value[1]
+
+        else:
+            return None
+
 
     """
     Adds the given key-value pair to the cache. The newly-
@@ -29,5 +43,52 @@ class LRUCache:
     want to overwrite the old value associated with the key with
     the newly-specified value.
     """
+    #adds key-value pair to cache
+    #assign it to the tail of the cache
+    #if cache is maxxed out delete head
+    #else if the key-value is already in the cache. assign it to the tail
+
     def set(self, key, value):
-        pass
+        if key in self.cache:
+
+            i = self.cache[key]
+            i.value =(key,value)
+            return self.storage.move_to_front(i)
+
+        if self.size == self.limit:
+            del self.cache[self.storage.tail.value[0]]
+            self.storage.remove_from_tail()
+            self.size -= 1
+        self.storage.add_to_head((key,value))
+        self.cache[key] = self.storage.head
+        self.size += 1
+
+
+
+
+
+
+
+
+
+
+
+
+
+        # if self.size == self.limit:
+        #     self.size -= 1
+        #     return self.storage.remove_from_head()
+        #
+        # if key in self.cache:
+        #     i = self.cache[key]
+        #     self.storage.add_to_tail(i)
+        # elif key not in self.cache:
+
+
+
+
+
+
+
+
+
